ce9c1f9f143e64e1438cd9f73c1bbc3d91d7bed9
surajkm20 <surajkm100120@gmail.com>
2024-07-21 00:55:06 +0530

    Time: 68 ms (8.99%), Space: 55.1 MB (88.74%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] heights = new int[cols + 1]; // Include an extra element for easier calculation
+        int maxArea = 0;
+        
+        for (char[] row : matrix) {
+            for (int i = 0; i < cols; i++) {
+                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+            }
+            
+            // Calculate max area using histogram method
+            int n = cols + 1; // Number of bars in the histogram
+            
+            for (int i = 0; i < n; i++) {
+                for (int j = i, minHeight = Integer.MAX_VALUE; j < n; j++) {
+                    minHeight = Math.min(minHeight, heights[j]);
+                    int mini = Math.min(minHeight, (j - i + 1));
+                    int area = mini * mini;
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        
+        return maxArea;
+    }
+}
