e6fcfa5ef3ef72950712a37078570c5eb33e34cf
surajkm20 <surajkm100120@gmail.com>
2024-09-11 23:19:13 +0530

    Time: 4 ms (97.14%), Space: 49.2 MB (19.81%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,31 +1,38 @@
 class Solution {
-    public int rec(String s, int ind1, String t, int ind2, int[][] dp){
-        // base
-        if(ind2 < 0) return 1;
-        else if(ind1 < 0) return 0;
-
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-
-        // match
-        int match = 0;
-        int notMatch = 0;
-        if(s.charAt(ind1) == t.charAt(ind2)){
-            match = rec(s, ind1-1, t, ind2-1, dp) + rec(s, ind1-1, t, ind2, dp);
-        }else{
-            notMatch = rec(s, ind1-1, t, ind2, dp);
+    public int rec(String s, String t, int ind1, int ind2, int[][] dp){
+        if(ind2 == t.length()){
+            return 1;
+        }
+        
+        if(ind1 == s.length()){
+            return 0;
+        }
+        if(s.length()-ind1<t.length()-ind2){
+            return 0;
         }
 
-        return dp[ind1][ind2] = match + notMatch;
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
+        }
+
+        // if match
+        int ans = 0;
+        if(s.charAt(ind1) == t.charAt(ind2)){
+            ans = rec(s, t, ind1+1, ind2+1, dp);
+        }
+        ans += rec(s, t, ind1+1, ind2, dp);
+
+        return dp[ind1][ind2] = ans;
     }
     public int numDistinct(String s, String t) {
-        int sL = s.length();
-        int tL = t.length();
-        int[][] dp = new int[sL][tL];
-        for(int i=0;i<sL;i++){
-            for(int j=0;j<tL;j++){
+        int sLength = s.length();
+        int tLength = t.length();
+        int[][] dp = new int[sLength][tLength];
+        for(int i=0;i<sLength;i++){
+            for(int j=0;j<tLength;j++){
                 dp[i][j] = -1;
             }
         }
-        return rec(s, sL-1, t, tL-1, dp);
+        return rec(s, t, 0, 0, dp);
     }
 }
